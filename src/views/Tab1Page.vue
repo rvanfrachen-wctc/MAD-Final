d2c0e0643b8438586946a98dbdd2cb91d0410c45
rvanfrachen-wctc <rvanfrachen@my.wctc.edu>
2022-12-07 15:14:00 -0600

    List on Home

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -15,10 +15,20 @@
       <div id="container">
         <p>Welcome to the photo gallery!</p>
         <br>
-        <p>On the second tab you can take pictures and save them within the app.</p>
-        <p>They are saved to your device so when you come back they will all be there.</p>
-        <p>You can also delete them if you no longer would like them to be saved.</p>
-        <p>This is great for saving select photos that you later want to share with family members or friends, you know right where to go.</p>
+        <ion-list :inset="true">
+          <ion-item>
+            <ion-label>On the second tab you can take pictures and save them within the app.</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-label>They are saved to your device so when you come back they will all be there.</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-label>You can also delete them if you no longer would like them to be saved.</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-label>This is great for saving select photos that you later want to share with family members or friends, you know right where to go.</ion-label>
+          </ion-item>
+        </ion-list>
       </div>
 
     </ion-content>
@@ -27,11 +37,11 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonList } from '@ionic/vue';
 
-export default  defineComponent({
+export default defineComponent({
   name: 'Tab1Page',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonItem, IonLabel, IonList }
 });
 </script>
 
